0fff632adb058d7a8df477454cf18ef60d8b662c
itizawa <itizawa@weseek.co.jp>
2020-07-04 17:17:56 +0900

    add hello world message

--- api/ogp.js
+++ api/ogp.js
@@ -1,10 +1,23 @@
-export default (req, res) => {
-  const params = getParams(req);
-  const imageBinary = await generateOgpImage(params);
+const { createCanvas } = require("canvas");
+
+export default async (req, res) => {
+  let { text } = req.query;
+  if (text == null) {
+    text = "hello world";
+  }
+
+  const canvas = createCanvas(1200, 630);
+  const context = canvas.getContext("2d");
+
+  context.font = "60px myfont";
+  context.fillStyle = "#ffffff";
+  context.fillText(text, 100, 100);
+
+  const image = canvas.toBuffer();
 
   res.writeHead(200, {
-    'Content-Type': 'image/png',
-    'Content-Length': imageBinary.length
-  })
-  res.end(imageBinary, 'binary')
+    "Content-Type": "image/png",
+    "Content-Length": image.length,
+  });
+  res.end(image, "binary");
 };
