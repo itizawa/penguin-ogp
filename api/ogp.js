4661641c6a7ac97d7e9f628302d6a88863d81f57
itizawa <itizawa@weseek.co.jp>
2020-07-04 16:31:45 +0900

    add settings

--- api/ogp.js
+++ api/ogp.js
@@ -1,3 +1,10 @@
 export default (req, res) => {
-  res.status(200).send("First api!!");
+  const params = getParams(req);
+  const imageBinary = await generateOgpImage(params);
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': imageBinary.length
+  })
+  res.end(imageBinary, 'binary')
 };
