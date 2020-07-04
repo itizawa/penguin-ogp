5819fb995fd70f313c771669df1ac218cda3e312
itizawa <itizawa@weseek.co.jp>
2020-07-04 21:12:36 +0900

    add new line settings

--- api/ogp.js
+++ api/ogp.js
@@ -4,26 +4,33 @@ const { createCanvas, registerFont, loadImage } = require("canvas");
 export default async (req, res) => {
   const { text } = req.query;
 
+  function splitByMeasureWidth(str, maxWidth, context) {
+    const lines = [];
+    let line = "";
+    str.split("").forEach((char) => {
+      line += char;
+      if (context.measureText(line).width > maxWidth) {
+        lines.push(line.slice(0, -1));
+        line = line.slice(-1);
+      }
+    });
+    lines.push(line);
+    return lines;
+  }
+
   async function generateImage(text) {
     const CANVAS_WIDTH = 1200;
     const CANVAS_HEIGHT = 630;
 
     const TEXT_COLOR = "#000000";
     const TEXT_SIZE = 72;
-    const FONT_FAMILY = "shirokuma-regular";
-    const FONT_PATH = path.join(
-      __dirname,
-      "..",
-      "fonts",
-      "shirokuma-regular.ttf"
-    );
+    const TEXT_LINE_MARGIN_SIZE = 16;
+    const TEXT_MARGIN_X = 68;
 
-    const BACKGROUND_IMAGE_PATH = path.join(
-      __dirname,
-      "..",
-      "images",
-      "ogp-background.jpg"
-    );
+    const FONT_FAMILY = "shirokuma-regular";
+    const FONT_PATH = path.join(__dirname, "..", "fonts", "shirokuma-regular.ttf");
+
+    const BACKGROUND_IMAGE_PATH = path.join(__dirname, "..", "images", "ogp-background.jpg");
     registerFont(FONT_PATH, { family: FONT_FAMILY });
     const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     const context = canvas.getContext("2d");
@@ -34,7 +41,15 @@ export default async (req, res) => {
 
     context.font = `${TEXT_SIZE}px ${FONT_FAMILY}`;
     context.fillStyle = TEXT_COLOR;
-    context.fillText(text, 100, 250);
+    const textLines = splitByMeasureWidth(text, CANVAS_WIDTH - TEXT_MARGIN_X, context);
+
+    let lineY = CANVAS_HEIGHT / 2 - ((TEXT_SIZE + TEXT_LINE_MARGIN_SIZE) / 2) * (textLines.length - 1);
+
+    textLines.forEach((line) => {
+      const textWidth = context.measureText(line).width;
+      context.fillText(line, (CANVAS_WIDTH - textWidth) / 2, lineY);
+      lineY += TEXT_SIZE + TEXT_LINE_MARGIN_SIZE;
+    });
 
     return canvas.toBuffer();
   }
